a9161fee2e9477ae5aede8181ad95114291bc939
Celina Jackson <65145705+CelinaJackson@users.noreply.github.com>
2021-01-29 10:54:21 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,4 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201111185846) do
 
-  create_table "statics", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-end
